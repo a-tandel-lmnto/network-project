eb510e70900c52234d79491e6a777e7e2970eb70
Aneep Tandel <a.tandel@lmn.to>
2026-08-05 18:00:10 +0400

    REmove ip from network

--- main.tf
+++ main.tf
@@ -1,15 +1,21 @@
-variable "public_ips" {
-    type = list(string)
-    default = [
-        "10.0.0.10",
-        "10.0.0.11",
-        "8.8.8.8",
-        "10.0.0.21",
-        "10.0.0.51"
-    ]
-}
+# variable "public_ips" {
+#     type = list(string)
+#     default = [
+#         "10.0.0.10",
+#         "10.0.0.11",
+#         "8.8.8.8",
+#         "10.0.0.21",
+#         "10.0.0.51"
+#     ]
+# }
 
-output "public_ips" {
-    description = "The public IPs of the instances"
-    value = var.public_ips
+# output "public_ips" {
+#     description = "The public IPs of the instances"
+#     value = var.public_ips
+# }
+
+
+resource "aws_instance" "web" {
+  ami           = "ami-02b64aa047cb5edf5"
+  instance_type = "t2.nano"
 }
